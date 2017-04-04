f9f052617f373cd87fa2766b0c651b4f443f8a3c
Tomasz Bialek <bialektomasz225@gmail.com>
2017-04-04 17:57:45 +0200

    CustomLogTab

---
+++ src/com/company/GUI/CustomLogTab.java
@@ -0,0 +1,36 @@
+package com.company.GUI;
+
+import com.example.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CustomLogTab extends Tab {
+
+    private final ListView<String> listView;
+
+    private final ObservableList<String> data;
+
+    public CustomLogTab() {
+        this.listView = new ListView<>();
+        this.data = FXCollections.observableArrayList();
+        this.listView.setItems(data);
+        this.setContent(this.listView);
+
+        this.setText("Log");
+        this.setClosable(false);
+    }
+
+    public synchronized void addData(String status, Student student) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
+
+        data.add(simpleDateFormat.format(date) + "        [" + status + "]          " +
+                student.getFirstName() + "        " + student.getLastName());
+    }
+}
